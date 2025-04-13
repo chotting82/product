8e1edfdaf7f3b1419c4e0998b9852a57023ef7ae
benjamin <chotting@gmail.com>
2025-04-13 14:45:13 +0900

    upload initial code

---
+++ main.py
@@ -0,0 +1,12 @@
+from decimal import Decimal
+
+
+if __name__ == '__main__':
+    a = 125
+
+    b = Decimal("3.669")
+    c = Decimal("1452.6700")
+
+    z = a/(b* c) * Decimal(100)
+
+    print(z)
